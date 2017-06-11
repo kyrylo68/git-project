60068f71076d868775661648994d405d3f841394
dmitry <dmitry@example.ru>
2017-06-11 17:56:36 +0300

    commit in to new branch

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world! changed9");
+		System.out.println("Hello world! changed10");
 	}
 }
